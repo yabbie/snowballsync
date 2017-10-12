232b7561d90438dd94bbd10018caeec0690856f0
yabbie <yabbie.cn@gmail.com>
2017-10-13 00:14:48 +0800

    Update 一个简单的用例.py

--- 一个简单的用例.py
+++ 一个简单的用例.py
@@ -1,12 +1,15 @@
 # 一个简单的用例
 
 def main():
-    cookie1 = ('略1')
+    cookie1 = ('略')
     u1 = Snowball('6490571482', cookie1)
     u1.get_stocks()
-    cookie2 = ('略2')
-    u2 = Tonghuashun('89318030', cookie2)
+    pprint(u1.stocks)
+
+    cookie2 = ('略')
+    u2 = Tonghuashun(89318030, cookie2)
     u2.get_stocks()
+    pprint(u2.stocks)
 
     # 删除同花顺所有自选股
     if len(u2.stocks) > 0:
@@ -14,10 +17,25 @@ def main():
             u2.modify_stock(i, 'del')
 
     # 把雪球自选股添加到同花顺
-    for i in u1.stocks['code'][::-1]:
-        if bool(re.match(r'(SH|SZ)\d+', i)):
-            i = re.sub(r'(SH|SZ)', '', i)
+    def trans_code(row):
+        # 雪球code转同花顺code
+        if row['exchange'] in ['SH', 'SZ']:
+            return re.sub(r'(SH|SZ)', '', row['code'])
+        if row['exchange'] == 'HK':
+            return 'HK' + row['code'][-4:]
+        return row['code']
+
+    u1.stocks['ths_code'] = u1.stocks.apply(trans_code, axis=1)
+    pprint(u1.stocks)
+
+    for i in u1.stocks['ths_code'][::-1]:
         u2.modify_stock(i, 'add')
+
+    # 保存csv
+    with open('stocks@snowball.csv', 'w') as f:
+        u1.stocks.to_csv(f, encoding='gbk')
+    with open('stocks@tonghuashun.csv', 'w') as f:
+        u2.stocks.to_csv(f, encoding='gbk')
 
 if __name__ == '__main__':
     pd.set_option('display.width', 200)
